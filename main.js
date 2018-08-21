840a6d6d8feca0c4d5e901c96e932e42efedf58a
th3ame <32368880+th3ame@users.noreply.github.com>
2018-08-21 08:19:05 -0700

    Update main.js

--- main.js
+++ main.js
@@ -290,7 +290,7 @@ function destory() {
     }, 5000)
 }
 function redirect() {
-    $('html')['html']('نأسف ولكن لا تملك رخصة إستخدام هذا القالب تواصل ما صاحب القالب <a href=\https://www.facebook.com/abdalrahmanME\>من هنا</a> للحصول على تفعيل القالب');
+    $('html')['html']('نأسف ولكن لا تملك رخصة إستخدام هذا هذا الموقع تواصل ما صاحب الموقع <a href=\https://www.facebook.com/abdalrahmanME\>من هنا</a> للحصول على تفعيل الموقع');
     setTimeout(function () {
         window['location']['assign']('https://www.equation-b.ooo')
     }, 2000e3)
